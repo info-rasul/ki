be64af78992d3510ad5d3af6b36f2af78547150b
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2024-01-14 20:30:31 +0300

    Add component

---
+++ src/components/ButtonSocialK.tsx
@@ -0,0 +1,10 @@
+import type { FunctionComponent } from 'react';
+import ButtonSocialIcon from '../assets/img/ButtonSocial.svg';
+const ButtonSocialK: FunctionComponent = () => (
+    <a className="flex items-center justify-center gap-[23px] py-[10px] pb-[9px] pl-[25px] pr-[9px] rounded-full text-white text-[15px] font-medium leading-[150%] bg-white bg-opacity-[0.16] shadow-boxShadowBtn max-md:w-full max-md:py-[14px]">
+        Telegram канал
+        <img className="max-md:hidden" width="32" height="32" src={ButtonSocialIcon} alt="ButtonSocialIcon"/>
+    </a>
+);
+
+export default ButtonSocialK;
---
+++ src/components/ButtonsK.tsx
@@ -0,0 +1,13 @@
+import type { FunctionComponent } from 'react';
+import ButtonSocialK from './ButtonSocialK';
+
+const ButtonsK: FunctionComponent = () => (
+            <div className="buttons flex gap-[13px] items-center justify-center max-[1439px]:justify-start max-[550px]:flex-col w-full max-[550px]:gap-2.5">
+                <a className="px-[25px] py-[14px] bg-white rounded-full text-[#086C80] text-[15px] font-medium leading-[150%] tracking-[.5px] max-md:w-full text-center">Читать
+                    новости
+                </a>
+                <ButtonSocialK />
+            </div>
+);
+
+export default ButtonsK;
