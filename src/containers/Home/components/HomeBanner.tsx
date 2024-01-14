bd08c3bf8c613f6c991ec6584b728126c1f8e0c5
a.rasul <a.rasul@flat-soft.ru>
2024-01-15 00:02:32 +0300

    KI-1 Add components

--- src/containers/Home/components/HomeBanner.tsx
+++ src/containers/Home/components/HomeBanner.tsx
@@ -1,12 +1,17 @@
 import type { FunctionComponent } from "react";
 import React, { Fragment } from "react";
 import oneSvg from "../../../assets/img/Subtract.png";
+// @ts-ignore
+import videoBack from "../../../assets/video/back.mp4";
 import PersonCard from "../../../components/PersonCard";
 
 const HomeBanner: FunctionComponent = () => (
     <Fragment>
         <section className="home-banner flex items-end h-[100vh] max-sm:h-[645px]">
-            <div className="w-full px-20 pb-[59px] max-lg:px-4 max-sm:pb-[30px]">
+            <video autoPlay muted loop id="myVideo">
+                <source src={videoBack} type="video/mp4" />
+            </video>
+            <div className="w-full px-20 pb-[59px] max-lg:px-4 max-sm:pb-[30px] z-10">
                 <h1 className="text-white text-[40px] font-medium leading-[140%] max-sm:text-[28px]">Трансформируем <br /> информацию в возможности </h1>
                 <div className="min-h-[1px] my-10 bg-[#FFFFFF]/[.20] max-sm:my-[25px]"></div>
                 <p className="text-white font-medium leading-[170%] max-sm:text-[14px] max-sm:leading-[150%]">
