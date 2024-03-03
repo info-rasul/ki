138d4f90e340bbb8f22a95a13df1902ecaadf800
a.rasul <a.rasul@flat-soft.ru>
2024-03-03 18:32:47 +0300

    Meta

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,10 +1,7 @@
 export { default as Home } from "./Home";
-export { default as About } from "./About";
 export { default as AboutUs } from "./AboutUs";
 export { default as Services } from "./Services";
-export { default as Blog } from "./Blog";
 export { default as Contacts } from "./Contacts";
-export { default as Article } from "./Article";
 export { default as QuestionsAndAnswers } from "./QuestionsAndAnswers";
 export { default as SupportOfCriminalCases } from "./SupportOfCriminalCases";
 export { default as ParallelImport } from "./ParallelImportNew";
